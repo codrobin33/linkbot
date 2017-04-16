f14ea7418e959a2bb78971e44717ca38d7801c6c
codrobin33 <cody@codybayrobinson.com>
2017-04-16 14:24:10 -0500

    can now get, save, and remove links

--- dbHelper.js
+++ dbHelper.js
@@ -5,55 +5,93 @@ var config = require('./config.js');
 
 // Connection URL
 var url = config.url;
-// Use connect method to connect to the Server
-/*MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-  db.close();
-});*/
 
 var insertLink = function(key, link, callback) {
   // connec to db
   MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    //console.log(arguments, {[key] : link});
+    //assert.equal(null, err);
+    if (err) {
+      callback({error:'error connecting to db.'});
+      return;
+    }
 
-    // Get the documents collection
+    // Get the links collection
     var collection = db.collection('links');
-    // Insert some documents
-    collection.insert({
-      key: key,
-      link: link
-    }, function(err, result) {
-      assert.equal(err, null);
-      //assert.equal(3, result.result.n);
-      //assert.equal(3, result.ops.length);
-      console.log("Inserted key into the links collection");
-      callback(result);
-    });
+
+    // before we insert, make sure we dont already have the key
+    collection.findOne({key: key}, function(error, result) {
+      if (error) {
+        callback({error: error});
+        return;
+      }
+
+      if (result) {
+        callback({error: 'key already exists'});
+        return;
+      }
+
+      collection.insert({
+        key: key,
+        link: link
+      }, function(error, result) {
+        if (error) {
+          callback({error:`error inserting ${link} to db.`});
+          return;
+        }
+
+        callback(result);
+      });
+    })
   })
 }
 
 var getLink = function(key, callback) {
   // connec to db
   MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
+    if (err) {
+      callback({error:'error connecting to db.'});
+      return;
+    }
 
     // Get the documents collection
     var collection = db.collection('links');
     // Find some documents
-    collection.find({key: key}).toArray(function(err, links) {
-      assert.equal(err, null);
-      //assert.equal(2, docs.length);
-      //console.log("Found the following links");
-      //console.dir(links);
-      callback(links);
+    collection.findOne({key: key}, function(err, link) {
+      if (err) {
+        callback({error:'error finding link.'});
+        return;
+      }
+      if (!link) {
+        callback({error:`${key} does not exist`});
+        return;
+      }
+      callback(link);
+    });
+  })
+}
+
+var removeLink = function(key, callback) {
+  // connec to db
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      callback({error:'error connecting to db.'});
+    }
+
+    // Get the documents collection
+    var collection = db.collection('links');
+    // Find some documents
+    collection.deleteOne({key: key}, function(err, link) {
+      if (err) {
+        callback({error:'error deleting key'});
+        return;
+      }
+      callback(link);
     });
   })
 }
 
 module.exports = {
   insert: insertLink,
-  get: getLink
+  get: getLink,
+  remove: removeLink
 }
--- index.js
+++ index.js
@@ -11,38 +11,75 @@ var rtm = new RtmClient(config.apiKey);
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
   if (message.text && message.text.includes(`<@${rtm.activeUserId}>`)) {
     // --- GET LINK ---
-    if (message.text.toLowerCase().includes('link me')) {
+    if (message.text.toLowerCase().includes('link me ')) {
       var link = message.text.toLowerCase().split('link me ')[1];
       console.log(`looking for ${link}`);
 
       dbHelper.get(link, function(result) {
-        console.log('callback: ', result);
-        if (result.length > 0 && typeof result[0].link !== 'undefined') {
-          rtm.sendMessage(`<@${message.user}>, ${result[0].link}`, message.channel);
+        if (!result) {
+          rtm.sendMessage(`<@${message.user}>, error: could not find link`, message.channel);
+          return;
+        } else if (result.error) {
+          rtm.sendMessage(`<@${message.user}>, error: ${result.error}`, message.channel);
+          return;
+        }
+
+        if (result && typeof result.link !== 'undefined') {
+          rtm.sendMessage(`<@${message.user}>, ${result.link}`, message.channel);
         } else {
           rtm.sendMessage(`<@${message.user}>, could not find the link.`, message.channel);
         }
-      })
+      });
+
+      return;
     }
 
     // --- SAVE LINK ---
-    if (message.text.toLowerCase().includes('save link')) {
+    if (message.text.toLowerCase().includes('save link ')) {
       var keyAndLink = message.text.toLowerCase().split('save link ')[1];
 
       var parts = keyAndLink.split(' ');
-      //console.log('parts, ', parts);
+
+      if (parts.length < 2) {
+        rtm.sendMessage(`<@${message.user}>, please enter your link like, facebook https://facebook.com`, message.channel);
+        return;
+      }
       var key = parts[0];
-      var link = parts[1].replace('<', '').replace('>', '');
+      var link = parts[1].replace('<', '').replace('>', ''); // slack adds these for some reason
 
       dbHelper.insert(key, link, function(result) {
-        console.log('callback insert: ', result);
+        if (result.error) {
+          rtm.sendMessage(`<@${message.user}>, error: ${result.error}`, message.channel);
+          return;
+        }
+
         rtm.sendMessage(`<@${message.user}>, inserted link ${link} with key ${key}`, message.channel);
-      })
+      });
+
+      return;
     }
 
     // --- UPDATE LINK ---
 
     // --- REMOVE LINK ---
+    if (message.text.toLowerCase().includes('remove link ')) {
+      var key = message.text.toLowerCase().split('remove link ')[1];
+
+      dbHelper.remove(key, function(result) {
+        if (result.error) {
+          rtm.sendMessage(`<@${message.user}>, error removing key ${key}`, message.channel);
+          return;
+        }
+
+        if (result.deletedCount === 1) {
+          rtm.sendMessage(`<@${message.user}>, removed key ${key}`, message.channel);
+        } else {
+          rtm.sendMessage(`<@${message.user}>, ${key} does not exist`, message.channel);
+        }
+      })
+
+      return;
+    }
   }
 });
 
